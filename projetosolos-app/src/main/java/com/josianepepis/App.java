e168d61d0ba9871b682407aa49e67f3b3d360a2c
Josiane Pepis <alves.a.josiane@gmail.com>
2021-03-04 09:27:36 -0300

    Criação do Projeto Maven
    Criação dos diretórios do projeto utilizando o maven-archetype-quickstart

---
+++ projetosolos-app/src/main/java/com/josianepepis/App.java
@@ -0,0 +1,13 @@
+package com.josianepepis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
